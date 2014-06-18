8837593c52fcd76d3c33694d078a28a436fa7600
palewire <ben.welsh@gmail.com>
2014-06-17 21:37:47 -0700

    Shell tests

---
+++ test.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import storytracker
+
+
+class BaseTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+
+class ArchiveTest(BaseTest):
+
+    def test_nothing(self):
+        pass
