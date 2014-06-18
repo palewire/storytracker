bd8348c01c2e3711b3873f6bf54c51528b4dfc9b
palewire <ben.welsh@gmail.com>
2014-06-17 21:25:35 -0700

    Barebones setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name='storytracker',
+      version='0.0.1',
+      description='',
+      author='Ben Welsh',
+      author_email='ben.welsh@gmail.com',
+      url='https://github.com/pastpages/storytracker',
+      packages=[''],
+)
