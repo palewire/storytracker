e93b0d0a28e70df6f21dbd71f7e9fe7b7fdfef45
Low Kian Seong <kianseong@gmail.com>
2014-06-18 14:16:24 +0800

    Update setup.py
    Add a line of description so that searches won't show `UNKNOWN` for description.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
       name='storytracker',
       version='0.0.1',
-      description='',
+      description='Story movement tracking for news homepages',
       author='Ben Welsh',
       author_email='ben.welsh@gmail.com',
       url='https://github.com/pastpages/storytracker',
