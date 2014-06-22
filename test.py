53b3623c3b559afc08f7bd7c83ea3664233d73e9
palewire <ben.welsh@gmail.com>
2014-06-21 22:29:05 -0700

    More tests

--- test.py
+++ test.py
@@ -16,6 +16,15 @@ class ArchiveTest(BaseTest):
     def test_nothing(self):
         html = storytracker.get("http://www.latimes.com")
         path = storytracker.archive(html)
+        try:
+            storytracker.get("http://www.trbimg.com/img-5359922b/turbine/la-me-lafd-budget-20140415-001/750/16x9")
+        except ValueError:
+            pass
+        storytracker.get(
+            "http://www.trbimg.com/img-5359922b/turbine/la-me-lafd-budget-20140415-001/750/16x9",
+            verify=False
+        )
+
 
 
 if __name__ == '__main__':
