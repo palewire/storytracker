71f7b98e6557b8a9c37cbb4aaa37b9e76f12053c
palewire <ben.welsh@gmail.com>
2014-08-03 20:27:30 -0700

    Some version fixes I hope that will make the six dependency safe for future generations

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ install_requires = [
     'python-dateutil>=2.2',
     'requests>=2.3.0',
     'htmlmin==0.1.5',
-    'six==1.7.2',
+    'six>=1.7.2',
     'pytz>=2014.4',
     'beautifulsoup4>=4.3.2',
-    'storysniffer==0.0.2',
+    'storysniffer==0.0.3',
     'selenium>=2.42.1',
 ]
 
