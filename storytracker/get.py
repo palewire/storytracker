ad2cd76ca04722b686465857211fd7af021eae12
charlex <charlex@Apollo.local>
2015-02-24 23:24:25 -0800

    Added 4xx and 5xx retries to get requests using user-agent header to overcome some servers' bot detection

--- storytracker/get.py
+++ storytracker/get.py
@@ -4,13 +4,20 @@ import requests
 logger = logging.getLogger(__name__)
 
 
-def get(url, verify=True):
+def get(url, verify=True, user_agent="PhantomJS"):
     """
     Retrieves HTML from the provided URL.
     """
     # Request the URL
     logger.debug("Requesting %s" % url)
     response = requests.get(url)
+
+    # If the reqest returns a 4XX or 5XX response, try again with a user agent
+    if not response.status_code == requests.codes.ok:
+        logger.debug("Request returned a %d error, trying again with %s user-agent" % (response.status_code, user_agent))
+        response = requests.get(url, headers={'User-Agent': user_agent})
+        logger.debug("Second Request returned a %d" % response.status_code)
+
     html = response.text
     # Verify that the response is in fact HTML (but option to skip test)
     if verify and 'html' not in response.headers['content-type']:
